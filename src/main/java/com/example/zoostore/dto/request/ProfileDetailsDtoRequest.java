0024542edd515bd0646e96437658983b5fba6691
Serikzhan Kuanyshev <70684876+SekoJuku@users.noreply.github.com>
2022-06-10 23:33:28 +0600

    profile dto

--- src/main/java/com/example/zoostore/dto/request/ProfileDetailsDtoRequest.java
+++ src/main/java/com/example/zoostore/dto/request/ProfileDetailsDtoRequest.java
@@ -1,17 +1,25 @@
 package com.example.zoostore.dto.request;
 
+import com.fasterxml.jackson.annotation.JsonIgnore;
+import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
+import com.fasterxml.jackson.annotation.JsonProperty;
+import com.fasterxml.jackson.annotation.JsonValue;
 import lombok.Data;
 
 @Data
+@JsonIgnoreProperties(ignoreUnknown = true)
 public class ProfileDetailsDtoRequest {
     private Long id;
 
     private String gender;
 
+    @JsonProperty("first_name")
     private String firstName;
 
+    @JsonProperty("last_name")
     private String lastName;
 
+    @JsonProperty("phone_number")
     private String phoneNumber;
 
     private String address;
